740063a709d17348eba1f7ac57f3ae28003ed600
Bruno Alves <alves.bruno945@gmail.com>
2021-07-04 19:28:53 -0300

    The initial setup

---
+++ Solve Me First/View/InputValues/InputValueViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InputValueViewController.swift
+//  Solve Me First
+//
+//  Created by Bruno Silva on 04/07/21.
+//
+
+import UIKit
+
+class InputValueViewController: UIViewController {
+    
+    //MARK: - Variables
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
